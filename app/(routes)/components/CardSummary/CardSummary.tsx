a11de2c5f870e8d715ce85b2bcfc67e5ca6440bf
Fernando Jorge Gregorio Perez <fernando24gregorio@gmail.com>
2024-05-10 18:07:15 +0200

    update

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -1,13 +1,16 @@
+import { CustomIcon } from "@/components/CustomIcon";
 import { CardSummaryProps } from "./CardSummary.types";
 
+
 export function CardSummary(props: CardSummaryProps) {
-    const {average, icon, title, tooltipText, total } = props 
+    const {average, icon: Icon, title, tooltipText, total } = props 
     
     return (
-     <div className="shadow-sm bg-background rounded-lg p-5 py-3 hover:shadow-lg transition">
-        <div className = "flex justify-between">
-            {title}
+        <div className="shadow-sm bg-background rounded-lg p-5 py-3 hover:shadow-lg transition">
+            <div className = "flex justify-between">
+                <CustomIcon icon={Icon}/>
+                {title}
+            </div>
         </div>
-     </div>
     )
 }
