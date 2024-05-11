8ae2f84b1f01123c515e40ba5868fe13110c81a1
Fernando Jorge Gregorio Perez <fernando24gregorio@gmail.com>
2024-05-11 11:21:32 +0200

    update

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -1,6 +1,6 @@
 import { CustomIcon } from "@/components/CustomIcon";
 import { CardSummaryProps } from "./CardSummary.types";
-
+import {CustomTooltip} from "@/components/CustomTooltip";
 
 export function CardSummary(props: CardSummaryProps) {
     const {average, icon: Icon, title, tooltipText, total } = props 
@@ -8,8 +8,13 @@ export function CardSummary(props: CardSummaryProps) {
     return (
         <div className="shadow-sm bg-background rounded-lg p-5 py-3 hover:shadow-lg transition">
             <div className = "flex justify-between">
-                <CustomIcon icon={Icon}/>
-                {title}
+                <div className = "flex gap-2 items-center">
+                    <CustomIcon icon={Icon}/>
+                    {title}
+                </div>
+                <CustomTooltip
+                    content = {tooltipText}
+                /> 
             </div>
         </div>
     )
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,31 @@
 import { Button } from "@/components/ui/button"
 import { UserButton } from "@clerk/nextjs";
 import { CardSummary } from "./components/CardSummary";
-import { UsersRound } from "lucide-react";
+import { BookOpenCheck, UsersRound, Waypoints, icons } from "lucide-react";
+
+export const dataCardsSummary = [
+  {
+    icon: UsersRound,
+    total: "12.450",
+    average: 15,
+    title: "Companies created",
+    tooltipText: "See all of the companies created"
+  },
+  {
+    icon: Waypoints,
+    total: "86.5%",
+    average: 80,
+    title: "Total Revenue",
+    tooltipText: "See all of the summary"
+  },
+  {
+    icon: BookOpenCheck,
+    total: "363,95€",
+    average:30,
+    title: "Bounce Rate",
+    tooltipText: "See all of the bounce rate"
+  },
+]
 
 export default function Home() {
   return (
@@ -9,15 +33,16 @@ export default function Home() {
       <UserButton />
       <h2 className="text-2xl mb-4">Dashboard</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
-        <CardSummary
-          icon={UsersRound}
-          total="12.450"
-          average={15}
-          title="Companies created "
-          tooltipText="See all of the companies created"
-        />
-        <div>Card Summary</div>
-        <div>Card Summary</div>
+        {dataCardsSummary.map(({icon, total, average, title, tooltipText}) => (
+          <CardSummary
+            key={title}
+            icon={icon}
+            total={total}
+            average={average}
+            title={title}
+            tooltipText={tooltipText}
+          />
+        ))}
       </div>
     </div>
   )
